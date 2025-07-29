06f706ccb9b3bbdfc8f8d7d27d2b441607819353
abhigorde15 <166307032+abhigorde15@users.noreply.github.com>
2025-07-29 08:23:50 +0530

    Project Structure organised

--- CampusConnect/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => { 
-  return(
-     <footer className="bg-gray-100 text-center text-sm text-gray-600 py-6">
-    <p>&copy; {new Date().getFullYear()} KKW Connect. Made for students by students.</p>
-  </footer>
-  )
- 
-}
-export default Footer;
