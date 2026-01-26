da8e549ae8b79943233e1f0cc5f49c28b9af18dd
abhigorde15 <166307032+abhigorde15@users.noreply.github.com>
2026-01-26 10:05:51 +0530

    Removed console logs

--- campus_connect/src/pages/LoginPage.jsx
+++ campus_connect/src/pages/LoginPage.jsx
@@ -22,7 +22,7 @@ const LoginPage = () => {
 
     try {
       const res = await loginUser(user);
-      console.log(res)
+   
       localStorage.setItem("token", res.token); 
       localStorage.setItem("userId", res.userId);
       toast.success("Login successful");
